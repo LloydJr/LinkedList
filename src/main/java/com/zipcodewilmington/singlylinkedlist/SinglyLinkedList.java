143dd90863b62c402ee74681557ae54e9ed703c4
LloydJr <98184266+LloydJr@users.noreply.github.com>
2022-07-24 03:44:50 -0400

    Completed Requirements

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 /**
  * Created by leon on 1/10/18.
  */
@@ -9,16 +11,21 @@ public class SinglyLinkedList {
      * Must have:
      * ===add===
      * ===remove===
-     * contains
+     * ===contains===
      * ===find===
-     * size
-     * get
-     * copy
-     * sort
+     * ===size===
+     * ===get===
+     * ===copy===
+     * ===sort===
      * ===Method Definition Complete===*/
 
-    int data;
+    Integer data;
     SinglyLinkedList next; //SinglyLinkedList == Node
+
+    public SinglyLinkedList(int data, SinglyLinkedList newNode) {
+        this.data = data;
+        this.next = newNode;
+    }
 
     /**Add*/
     public void addSinglyAtFront(SinglyLinkedList newNode, SinglyLinkedList head){
@@ -62,6 +69,10 @@ public class SinglyLinkedList {
         }
         temp.next = null;
     }
+    public Boolean contains(SinglyLinkedList valueToContain) {
+        if (next == null) return false;
+        else return next.contains(valueToContain);
+    }
     /**Find*/
     public SinglyLinkedList find (SinglyLinkedList head, int target) {
         SinglyLinkedList temp = head;
@@ -69,5 +80,55 @@ public class SinglyLinkedList {
             temp = temp.next;
         }
         return temp;
+    }
+    /**Size*/
+    public Integer size (SinglyLinkedList head) {
+        SinglyLinkedList temp = head;
+        Integer count = 0;
+        while (temp != null) {
+            count++; temp = temp.next;
+        }
+        return count;
+    }
+    /**Get*/
+    public SinglyLinkedList get () {
+        return this.next;
+    }
+    /**Copy*/
+    public SinglyLinkedList copy (SinglyLinkedList head) {
+        SinglyLinkedList temp = head;
+        SinglyLinkedList newNode = null;
+        SinglyLinkedList tail = null;
+
+        while (temp != null) {
+            if (newNode == null) {
+                newNode = new SinglyLinkedList(temp.data, newNode);
+                tail = newNode;
+            }
+            else {
+                tail.next = new SinglyLinkedList(temp.data, tail.next);
+                tail = tail.next;
+            }
+        }
+        return newNode;
+    }
+    /**Sort*/
+    public void sort (SinglyLinkedList head) {
+        SinglyLinkedList current = head, index = null;
+        Integer temp;
+        if (head == null) {
+            return;
+        }
+        else {
+            while (current != null) {
+                if (current.data > index.data) {
+                    temp = current.data;
+                    current.data = index.data;
+                    index.data = temp;
+                }
+                index = index.next;
+            }
+            current = current.next;
+        }
     }
 }
